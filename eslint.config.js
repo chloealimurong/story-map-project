6d3b6a9f66c116e15058af890a0e6fd0548b0551
Chloe Alimurong <chloebalimurong@gmail.com>
2026-09-20 22:53:20 -0400

    linter test

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
-import js from "@eslint/js";
-import globals from "globals";
+import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { ignores: ["**/*.js", "**/*.cjs", "**/*.mjs"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
 ]);
